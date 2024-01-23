e9ed387835710344bcba4fd95d139958e6845efe
Mustafa Atak <mustafa.atak@boun.edu.tr>
2024-01-24 02:47:32 +0300

    All companies fetched

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 from fastapi import FastAPI
-from tasks import fetch_company_details  
+from fastapi.responses import JSONResponse
+from print_schema import print_schema
+from celery import group, chain
+from celery.result import GroupResult
+
 import requests
 import json
-from celery import group
-from print_schema import print_schema
-from celery.result import GroupResult
+
+from tasks import fetch_company_details, fetch_all_companies
 from tasks import app as celery_app
-from fastapi.responses import JSONResponse
-
-import io
-import sys
-def capture_print_schema_to_html(data):
-    # Capture the output as before
-    buffer = io.StringIO()
-    old_stdout = sys.stdout
-    sys.stdout = buffer
-    print_schema(data)
-    sys.stdout = old_stdout
-    content = buffer.getvalue()
-    buffer.close()
-
-    # # Convert the captured output to HTML
-    # html_content = content.replace('\n', '<br>').replace('\t', '&nbsp;' * 4)
-    return content
-
-from host import headers, first_request_data
 
 app = FastAPI()
 
 scraping = False
-status = None
 scraperId = None
 
 @app.get("/")
@@ -42,50 +25,46 @@ async def scraper():
 
     # Code to start the Celery tasks
     if not scraping:
-        response = requests.post('https://ranking.glassdollar.com/graphql', headers=headers, json=first_request_data)
-        top_companies = response.json()['data']['topRankedCorporates']
         print("Scaping started")
 
+        # Creating a group of tasks for fetching companies on each page
+        fetch_tasks = group(fetch_all_companies.s(page) for page in range(1, 28))
+
+        # Executing the group asynchronously and storing the group result
+        group_result = fetch_tasks.apply_async()
+        group_result.save()
+
+
+        # Waiting for all fetch_all_companies tasks to complete
+        all_companies = []
+        for async_result in group_result.get():
+            if async_result:
+                all_companies.extend(async_result)
+
         # Queue tasks for each company ID using Celery
-        tasks = [fetch_company_details.s(company['id']) for company in top_companies]
-        job = group(tasks)  # group the tasks to be executed in parallel
-        result = job.apply_async(countdown=2)  # apply_async will send the tasks to the workers
-        result.save()
+        detail_tasks = [fetch_company_details.s(company['id']) for company in all_companies]
+        details_job = group(detail_tasks)
+        details_result = details_job.apply_async(countdown=0)
+        details_result.save()
         scraping = True
-        scraperId = result.id
+        scraperId = details_result.id
+
+
         # status = fetch_company_details.AsyncResult(result.id).status
         return {"status": "Scraping started, please come back later for results."}
     
     else:
         # Restore the GroupResult object using the saved ID
-        group_result = GroupResult.restore(scraperId, app=celery_app)
+        details_result = GroupResult.restore(scraperId, app=celery_app)
 
-        if not group_result:
-            return {"status": "Invalid job ID or job may have expired"}
-
-        # Check if all tasks in the group are completed
-        if group_result.ready():
-            scraping = False
-            # Retrieve the results of all tasks
-            results = group_result.get()
-            # Optionally, you can format the results as JSON
-            formatted_results = json.dumps(results, indent=4)
-            # Print the results (or return them in the response)
-            print_schema(results)
-
-            return {"status": "All tasks completed", "results len": len(results) }
-        else:
-            return {"status": "Tasks still running"}
+        if not details_result or not details_result.ready():
+            return {"status": "Scraping is still in progress"}
+        
+        scraping = False
+        # details_results = [result.get() for result in group_result.children]
+        details_results = details_result.get()
+        print_schema(details_results)
+        names = [company.get('name') for company in details_results if company and 'twitter_url' in company]
 
 
-
-
-# # # Optional: Wait for all tasks to finish and collect the results
-# # company_details = result.join()  # This will block until all tasks are done
-
-# # Format the data into JSON
-# formatted_data = json.dumps(company_details, indent=4)
-# # You can print or save the formatted data as needed
-
-# print(len(company_details))
-# print_schema(company_details, indent=3, dense=False)
+        return JSONResponse(content={"status": "All tasks completed", "results": names}, media_type="application/json")
--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from celery import Celery
+from celery import Celery, chord
 from celery.utils.log import get_task_logger
 
 app = Celery('CompanyAnalyzer', broker='pyamqp://guest@localhost//', backend='redis://localhost')
@@ -27,9 +27,32 @@ def fetch_company_details(company_id):
         'variables': {
             'id': company_id,
         },
-        'query': 'query ($id: String!) {\n  corporate(id: $id) {\n    id\n    name\n    description\n    logo_url\n    hq_city\n    hq_country\n    website_url\n    linkedin_url\n    twitter_url\n    startup_partners_count\n    startup_partners {\n      master_startup_id\n      company_name\n      logo_url: logo\n      city\n      website\n      country\n      theme_gd\n      __typename\n    }\n    startup_themes\n    startup_friendly_badge\n    __typename\n  }\n}\n',
+        'query': 'query ($id: String!) {\n  corporate(id: $id) {\n    id\n    name\n    description\n    logo_url\n    hq_city\n    hq_country\n    website_url\n    linkedin_url\n    twitter_url\n    startup_partners_count\n    startup_partners {\n      master_startup_id\n      company_name\n      logo_url: logo\n      city\n      website\n      country\n      theme_gd\n     }\n    startup_themes\n    }\n}\n',
     }
     response = requests.post('https://ranking.glassdollar.com/graphql', headers=headerDetailed, json=json_data)
     return response.json()['data']['corporate']
 
-
+@app.task(name='tasks.fetch_all_companies')
+def fetch_all_companies(pageNo):
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'tr-TR,tr;q=0.6',
+        'content-type': 'application/json',
+        'origin': 'https://ranking.glassdollar.com',
+        'referer': 'https://ranking.glassdollar.com/',
+    }
+    
+    json_data = {
+        'query': 'query GetCorporates($page: Int, $filters: CorporateFilters, $sortBy: String) {\n  corporates(page: $page, filters: $filters, sortBy: $sortBy) {\n    rows {\n      id\n     }\n  }\n}\n',
+        'variables': {
+            'page': pageNo, 
+            'filters': {
+                'hq_city': [],
+                'industry': [],
+            },
+            'sortBy': '',
+        },
+    }
+    
+    response = requests.post('https://ranking.glassdollar.com/graphql', headers=headers, json=json_data)
+    return response.json()['data']['corporates']['rows']
